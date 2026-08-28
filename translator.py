33d2909fc64db740213bbfc2a9725370649c1820
mathewjohann-ship-it <mathew.johann@gmail.com>
2026-08-28 16:28:30 +0100

    Add files via upload

--- translator.py
+++ translator.py
@@ -1,6 +1,11 @@
 import speech_recognition as sr
 import pyttsx3
 from googletrans import Translator
+import subprocess
+
+def speak_hindi(text):
+
+    subprocess.run(["say", "-v", "Lekha", text])
 
 def speak(text, language="en"):
     engine = pyttsx3.init()
@@ -31,9 +36,46 @@ def speech_to_text():
     except sr.RequestError as e:
         print(f"API Error: {e}")
         return ""
+# def translate_text(text, target_language="es"):
+#     translator = Translator()
+#     translation = translator.translate(text, dest=target_language)
+async def translate_text(text, target_language):
+    try:
+        translator = Translator()
+        translation = await translator.translate( text, dest=target_language )
+        print(f"🌍 Translated text: {translation.text}") 
+        return translation.text 
+    except Exception as e:
+        print(f"❌ Translation error: {e}")
+        return ""
+def display_language_options():
+    print("???? Available translation languages: ")
+    print("1. Hindi (hi)")
+    print("2. Bengali (bn)")
+    print("3. Marathi (mr)")
+    print("4. Malayalam (ml)")
 
-def translate_text(text, target_language = "es"):
-    translator = Translator()
-    translation = translator.translate(text, dest=target_language)
-    print(f"???? Translated text: {translation.text}")
-    return translation.text
+    choice = input("Please select the target language number (1 - 8): ")
+    language_dict = {
+        "1": "hi",
+        "2": "bn",
+        "3": "mr",
+        "4": "ml"
+    }
+    return language_dict.get(choice, "es")
+
+async def main():
+    target_language = display_language_options()
+    original_text = speech_to_text()
+    if original_text:
+        translated_text = await translate_text(original_text, target_language = target_language)
+        if translated_text:
+            speak(translated_text, language = target_language)
+            print("Translation spoken out!")
+
+            speak_hindi(translated_text)
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
